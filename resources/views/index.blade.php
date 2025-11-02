faa0ff3a2fb035be2427ef891e05be35f6e04ef4
dopy <dopymonster@gamil.com>
2025-11-02 14:53:13 +0800

    setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends("layout.master")
+
+@section("content")
+    <h1 class=""></h1>
+    <h1 class="p"></h1>
+    
+    <script>
+        let sd = document.querySelectorAll
+    </script>
+
+@endsection
